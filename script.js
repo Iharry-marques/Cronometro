e4ffaf8fddda717d45d4c3e27ddc4b189e094483
fmmoliveira <fabio.marques.melo@gmail.com>
2023-06-13 13:13:37 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const minutesEl = document.querySelector("#minutes")
+const secondsEl = document.querySelector("#seconds")
+const millisecondsEl = document.querySelector("#milliseconds")
+const startBtn = document.querySelector("#startBtn")
+const pauseBtn = document.querySelector("#pauseBtn")
+const resumeBtn = document.querySelector("#resumeBtn")
+const resetBtn = document.querySelector("#resetBtn")
